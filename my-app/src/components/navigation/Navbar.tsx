cd258f0aec858b652a4b22ac3f3356d11567a3ab
Elisabeth <elise.doisneau@gmail.com>
2024-07-14 12:36:40 +0200

    Update Navbar.tsx

--- my-app/src/components/navigation/Navbar.tsx
+++ my-app/src/components/navigation/Navbar.tsx
@@ -2,44 +2,57 @@ import React from 'react'
 import { Home, Folder, Briefcase, GitHub, Mail, FilePlus } from 'react-feather'
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from '../ui/modeToggle'
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <div className='w-full h-full flex justify-between items-center px-4 py-2 rounded-xl border m-4'>
+        <div className='w-full h-full max-w-lg  flex justify-between items-center px-4 py-2 rounded-xl border m-4'>
             <div className='flex justify-center items-center gap-2'>
                 {/* Home */}
+                <Link href="#home"> 
                 <Button variant="ghost" size="icon" aria-label="Home">
                     <Home />
                 </Button>
+                </Link>
 
                 {/*Projects*/}
+                <Link href="#home">
                 <Button variant="ghost" size="icon" aria-label="Projects">
                     <Folder/>
                 </Button>
+                </Link>
 
                 {/* Experience */}
+                <Link href="#home">
                 <Button variant="ghost" size="icon" aria-label="Experience">
                     <Briefcase/>
                 </Button>
+                </Link>
 
                 {/* Education */}
+                <Link href="#home">
                 <Button variant="ghost" size="icon" aria-label="Education">
                     <GitHub />
                 </Button>
+                </Link>
 
                 {/* Contact */}
+                <Link href="#home">
                 <Button variant="ghost" size="icon" aria-label="Contact">
                     <Mail />
                 </Button>
+                </Link>
             </div>
             <div className='flex justify-center items-center gap-2'>
                 {/* Toggle theme*/}
                 <ModeToggle />
                 {/* Hire me */}
+                <Link href="#home">
                 <Button className='flex justify-center items-center gap-2' variant="outline" aria-label="Hire me" >
                  <FilePlus/>
                     Hire me
                 </Button>
+                </Link>
             </div>
 
         </div>
