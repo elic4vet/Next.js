58f2e69854a9a6ff5d91ab66c0542fb9710f0455
Elisabeth <elise.doisneau@gmail.com>
2024-07-14 13:37:59 +0200

    Update page.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Section from '@/components/sections';
+import Section from '@/components/Sections';
+import ItemCard from '@/components/ItemCard'
 import { Folder } from 'react-feather';
+
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
       <Section title="Projects" icon={<Folder />}>
-        <div className='w-full h-full m-2 max-w-lg text-center flex-col justify-center items-center gap-2'> Hi</div>
+        <ItemCard image='/next.jpeg' title={"NextJs Portfolio"} description={"Portfolio"} tags={["Nextjs", "shadcn/ui" ,"Tailwind" , "React"]} href={"/projects/portfolio"}/> 
       </Section>
     </main>
   );
