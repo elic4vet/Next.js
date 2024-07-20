9380b0b72223b1cf4b25d62eaaee504fc2ea0362
Elisabeth <elise.doisneau@gmail.com>
2024-07-20 13:59:56 +0200

    Update Sections.tsx

--- my-app/src/components/Sections.tsx
+++ my-app/src/components/Sections.tsx
@@ -8,12 +8,12 @@ type Props = {
 
 export default function Section({ title, icon, children }: Props) {
     return (
-        <div className='max-w-lg w-full h-full flex flex-col justify-center items-start bg-secondary'>
-            <span className='flex justify-center items-center gap-1 m-2 p-2 text-lg font-semibold'>
+        <div className='max-w-lg w-full h-full flex flex-col justify-center items-start bg-secondary space-y-4'>
+            <span className='flex justify-center items-center mb-0 gap-1 m-2 p-2 text-lg font-semibold'>
                 {icon}
                 <h2>{title}</h2>
             </span>
-            <div>
+            <div className='w-full h-full m-2 mt-0 flex flex-col justify-center items-center'>
                 {children}
             </div>
         </div>
