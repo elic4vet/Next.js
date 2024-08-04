4591a26f2efd587d291d1ebc4d2bb3ac065b32ea
Elisabeth <elise.doisneau@gmail.com>
2024-08-04 15:24:20 +0200

    +sticky navbar

--- my-app/src/components/navigation/Navbar.tsx
+++ my-app/src/components/navigation/Navbar.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <div className='w-full h-full max-w-lg  flex justify-between items-center px-4 py-2 rounded-xl border m-4'>
+        <div className='w-full h-full bg-primary-foreground max-w-lg flex justify-between items-center px-4 py-2 rounded-xl border m-0'>
             <div className='flex justify-center items-center gap-2'>
                 {/* Home */}
                 <Link href="#home"> 
@@ -16,28 +16,28 @@ const Navbar = () => {
                 </Link>
 
                 {/*Projects*/}
-                <Link href="#home">
+                <Link href="#projects">
                 <Button variant="ghost" size="icon" aria-label="Projects">
                     <Folder/>
                 </Button>
                 </Link>
 
                 {/* Experience */}
-                <Link href="#home">
+                <Link href="#experience">
                 <Button variant="ghost" size="icon" aria-label="Experience">
                     <Briefcase/>
                 </Button>
                 </Link>
 
                 {/* Education */}
-                <Link href="#home">
+                <Link href="#education">
                 <Button variant="ghost" size="icon" aria-label="Education">
                     <GitHub />
                 </Button>
                 </Link>
 
                 {/* Contact */}
-                <Link href="#home">
+                <Link href="#contact">
                 <Button variant="ghost" size="icon" aria-label="Contact">
                     <Mail />
                 </Button>
