36d7aaf83940f6dfc0b37b69727516b428484bb8
Elisabeth <elise.doisneau@gmail.com>
2024-08-04 15:24:23 +0200

    Update NavbarContainer.tsx

--- my-app/src/components/navigation/NavbarContainer.tsx
+++ my-app/src/components/navigation/NavbarContainer.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 export default function NavbarContainer({ }: Props) {
   return (
-    <div className="flex flex-col items-center justify-between">
+    <div className="flex flex-col items-center justify-between top-0 sticky z-50">
       <Navbar />
     </div>
       );
