77e6fee89dec3a2a288eba264cb89f352e1bf770
Elisabeth <elise.doisneau@gmail.com>
2024-08-22 20:41:32 +0200

    Update Navbar.tsx

--- my-app/src/components/navigation/Navbar.tsx
+++ my-app/src/components/navigation/Navbar.tsx
@@ -1,46 +1,55 @@
 import React from 'react'
-import { Home, Folder, Briefcase, GitHub, Mail, FilePlus } from 'react-feather'
+import { Home, Folder, Briefcase, GitHub, Mail, FilePlus, Book } from 'react-feather'
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from '../ui/modeToggle'
 import Link from 'next/link'
+import { GraduationCap} from 'lucide-react';
+ 
 
 const Navbar = () => {
     return (
         <div className='w-full h-full bg-primary-foreground max-w-lg flex justify-between items-center px-4 py-2 rounded-xl border m-0'>
             <div className='flex justify-center items-center gap-2'>
                 {/* Home */}
-                <Link href="#home"> 
-                <Button variant="ghost" size="icon" aria-label="Home">
-                    <Home />
-                </Button>
+                <Link href="#home">
+                    <Button variant="ghost" size="icon" aria-label="Home">
+                        <Home />
+                    </Button>
                 </Link>
 
                 {/*Projects*/}
                 <Link href="#projects">
-                <Button variant="ghost" size="icon" aria-label="Projects">
-                    <Folder/>
-                </Button>
+                    <Button variant="ghost" size="icon" aria-label="Projects">
+                        <Folder />
+                    </Button>
                 </Link>
 
                 {/* Experience */}
                 <Link href="#experience">
-                <Button variant="ghost" size="icon" aria-label="Experience">
-                    <Briefcase/>
-                </Button>
+                    <Button variant="ghost" size="icon" aria-label="Experience">
+                        <Briefcase />
+                    </Button>
                 </Link>
 
                 {/* Education */}
-                <Link href="#education">
-                <Button variant="ghost" size="icon" aria-label="Education">
-                    <GitHub />
-                </Button>
+                <Link href="#education" target='_blank'>
+                    <Button variant="ghost" size="icon" aria-label="Education">
+                    <GraduationCap/>
+                    </Button>
+                </Link>
+
+                   {/* Github */}
+                   <Link href="https://github.com/elic4vet" target='_blank'>
+                    <Button variant="ghost" size="icon" aria-label="Github">
+                        <GitHub />
+                    </Button>
                 </Link>
 
                 {/* Contact */}
                 <Link href="#contact">
-                <Button variant="ghost" size="icon" aria-label="Contact">
-                    <Mail />
-                </Button>
+                    <Button variant="ghost" size="icon" aria-label="Contact">
+                        <Mail />
+                    </Button>
                 </Link>
             </div>
             <div className='flex justify-center items-center gap-2'>
@@ -48,10 +57,10 @@ const Navbar = () => {
                 <ModeToggle />
                 {/* Hire me */}
                 <Link href="https://www.linkedin.com/in/eerkekoglou/" target='_blank'>
-                <Button className='flex justify-center items-center gap-2' variant="outline" aria-label="Hire me" >
-                 <FilePlus/>
-                    Hire me
-                </Button>
+                    <Button className='flex justify-center items-center gap-2' variant="outline" aria-label="Hire me" >
+                        <FilePlus />
+                        Hire me
+                    </Button>
                 </Link>
             </div>
 
