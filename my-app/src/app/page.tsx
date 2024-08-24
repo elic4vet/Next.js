ccbe0dfcb27a7834bcd8a9b920e55aee1677fa82
Elisabeth <elise.doisneau@gmail.com>
2024-08-24 20:05:01 +0200

    Update page.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -23,10 +23,11 @@ export default function Home() {
           location={'Germany'}
           email={'elisabeth.erkekoglou@gmail.com'}
           image={'/me.png'}
+          
           currentCompany={{
             name: 'Arvato SE',
             role: 'Costumer Service Employee',
-            duration: 'Since October 2016'
+            duration: 'since October 2016'
           }}
           resume={'https://docs.google.com/document/d/19mXeur9GGbbMddEzv2haSGYLkGMKSfH9/edit?usp=sharing&ouid=115227731188943947624&rtpof=true&sd=true'}
         />
