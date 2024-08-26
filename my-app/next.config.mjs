30a7baad28cc879c263c3041912f600721af4157
Elisabeth <elise.doisneau@gmail.com>
2024-08-26 20:29:54 +0200

    plugins

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/Next.js",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
